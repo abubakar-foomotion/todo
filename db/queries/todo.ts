ce373ccab8f2cc4d3e99d1d43dcdd46f762f6c98
abubakar-foomotion <abubakar@foomotion.io>
2026-04-02 09:51:39 +0000

    refactor

--- db/queries/todo.ts
+++ db/queries/todo.ts
@@ -5,9 +5,8 @@ import {todos} from "../schema";
 
 export async function getAllTodos() {
     const res = await db.select().from(todos);
-    console.log(res);
     return res;
-    // return await db.select().from(todos);
+
 }
 
 export async function addTodo(userId: string, heading: string, description: string) {
