d5c9d556c466f8e74a762726a141117b90b755dc
abubakar-foomotion <abubakar@foomotion.io>
2026-04-03 06:59:26 +0000

    add basic protection of pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
+"use client";
+import { useUserStore } from "@/lib/store/userStore";
 
 import "./globals.css";
+import Welcome from "./welcome/page";
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const { userId } = useUserStore();
   return (
-    <html
-      lang="en"
-      
-    >
-      <body className="">
-        
-        {children}</body>
+    <html lang="en">
+      <body>{userId === "" ? <Welcome /> : <div>{children}</div>}</body>
     </html>
   );
+
 }
