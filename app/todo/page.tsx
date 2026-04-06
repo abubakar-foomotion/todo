54dac2bab6684c79cfc4c327ce2ec40b679d7a63
abubakar-foomotion <abubakar@foomotion.io>
2026-04-06 09:32:39 +0000

    change the orientation of todo list

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -29,12 +29,12 @@ export default function Todo() {
 
   return (
     <div>
-      <h1 className="font-bold text-center text-2xl">
-        this is todo Application..!!
+      <h1 className="font-bold text-center text-4xl">
+        TODOLOGYY
       </h1>
 
       {items.length > 0 && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-4">
+        <div className="grid grid-cols-1 gap-4 p-4">
           {items.map(({ heading, description, id }) => {
             if (id === udpdateId) {
               return (
